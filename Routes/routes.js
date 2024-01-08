7b466f1ac104a62587e3a050c987a839cdd3ee49
Gabriel Silva <gabrielback2009@hotmail.com>
2024-01-07 21:24:22 -0300

    Definindo rotas

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,6 +1,32 @@
 import express from 'express';
 const router = express.Router();
 
+//Array para armazenar os posts 
+let posts = [];
+
+//Rota para exibir formulário de criação de posts
+router.get("/", (req,res) => {
+    res.render('index')
+});
+
+
+//Roata post para criação de posts novos
+router.post('/posts', (req, res) => {
+    const newPost = {
+        id: posts.length + 1,
+        content: req.body.postContent
+    };
+
+    posts.push(newPost);
+    res.redirect('/');
+});
+
+//Exibir os posts criados
+router.get('/posts', (req, res) => {
+    res.render('posts', { posts });
+});
+
+
 
 // Exporta o objeto router
 module.exports = router;
--- app.js
+++ app.js
@@ -5,8 +5,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
   });
